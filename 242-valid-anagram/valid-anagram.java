b4cdf516e7a3feb94382c940c18fe61ae105a1cb
NISHANT PRATAP SINGH <babuannishant@gmail.com>
2026-09-08 20:16:18 +0530

    Time: 3 ms (94.12%) | Memory: 46.6 MB (25.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr=new int[26];
-        if(s.length()!=t.length()) return false;
-        for(int i=0; i<s.length(); i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+        char[] s1=s.toCharArray();
+        char[] t1=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        if(Arrays.equals(t1, s1)){
+            return true;
         }
-        for(int i=0; i<26; i++){
-            if(arr[i]!=0) {
-                return false;
-            }
-        }
-        return true;
+        else return false;
+
     }
 }
+
+
+
+
+
+
+//////2nd approach
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         int[] arr=new int[26];
+//         if(s.length()!=t.length()) return false;
+//         for(int i=0; i<s.length(); i++){
+//             arr[s.charAt(i)-'a']++;
+//             arr[t.charAt(i)-'a']--;
+//         }
+//         for(int i=0; i<26; i++){
+//             if(arr[i]!=0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }

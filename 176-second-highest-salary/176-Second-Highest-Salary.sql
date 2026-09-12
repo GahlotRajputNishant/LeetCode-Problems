8a89eb5f5e71640eb37a62d0e5e63236de792d42
NISHANT PRATAP SINGH <babuannishant@gmail.com>
2026-09-12 16:44:19 +0530

    [176] [Time Beats: 94.18%] - LeetPush

---
+++ 176-second-highest-salary/176-Second-Highest-Salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT(SELECT MAX(salary) FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee))AS SecondHighestSalary;
+
+-- SELECT (
+--     SELECT MAX(salary)
+--     FROM Employee
+--     WHERE salary < (
+--         SELECT MAX(salary)
+--         FROM Employee
+--     )
+-- ) AS SecondHighestSalary;

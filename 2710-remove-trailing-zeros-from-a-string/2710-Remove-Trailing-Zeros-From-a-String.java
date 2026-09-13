2d2a595d9e3d7bf25e2deb302fd440b80719a656
NISHANT PRATAP SINGH <babuannishant@gmail.com>
2026-09-13 08:33:40 +0530

    [2710] [Time Beats: 100.00%] [Memory Beats: 34.90%] - LeetPush

--- 2710-remove-trailing-zeros-from-a-string/2710-Remove-Trailing-Zeros-From-a-String.java
+++ 2710-remove-trailing-zeros-from-a-string/2710-Remove-Trailing-Zeros-From-a-String.java
@@ -1,11 +1,35 @@
 class Solution {
     public String removeTrailingZeros(String num) {
-        StringBuilder sb=new StringBuilder(num);
-        int i=sb.length()-1;
-        while(i>=0&& sb.charAt(i)=='0'){
-            sb.deleteCharAt(i);
-            i--;
+        int n=num.length();
+        int j=0;
+        for(int i=n-1; i>=0; i--){
+            char c=num.charAt(i);
+            if(c!='0'){
+                j=i;
+                break;
+            }
         }
-        return sb.toString();
+        return num.substring(0, j+1);
     }
 }
+
+
+
+
+
+////////// 2nd Approach
+
+
+
+
+// class Solution {
+//     public String removeTrailingZeros(String num) {
+//         StringBuilder sb=new StringBuilder(num);
+//         int i=sb.length()-1;
+//         while(i>=0&& sb.charAt(i)=='0'){
+//             sb.deleteCharAt(i);
+//             i--;
+//         }
+//         return sb.toString();
+//     }
+// }

ad574906cfb794d7e374f54dffd480975889729e
NISHANT PRATAP SINGH <babuannishant@gmail.com>
2026-09-13 08:27:26 +0530

    [2710] [Time Beats: 29.29%] [Memory Beats: 15.38%] - LeetPush

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-Remove-Trailing-Zeros-From-a-String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        StringBuilder sb=new StringBuilder(num);
+        int i=sb.length()-1;
+        while(i>=0&& sb.charAt(i)=='0'){
+            sb.deleteCharAt(i);
+            i--;
+        }
+        return sb.toString();
+    }
+}
